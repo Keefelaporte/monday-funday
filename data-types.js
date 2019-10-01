6e470abfdfff16e215fdd16b25bf9d7e9d567800
Keefe Laporte <keefelaporte2019@gmail.com.com>
2019-09-30 21:14:41 -0400

    My personal

---
+++ data-types.js
@@ -0,0 +1,17 @@
+var name = 'Keefe'
+var age = 41;
+var race ='Black'
+var job = 'Driver'
+var gender = 'Male'
+var weight = 200;
+var hobbies = ['Coding ', 'Basketball ', 'Chess ', 'Cards ']
+var year_born = 1978;
+let myobj = {}
+let me = {
+    character: 'Bold',
+    bench : 425
+}
+var today= new Date()
+var xMas2018 = new Date('2018-12-25')
+console.log (hobbies[0] + ' ' + hobbies[1] + ' ' + hobbies[2])
+console.log (name = 'Hello my name is ' + name )
