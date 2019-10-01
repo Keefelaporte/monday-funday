57ec7e87e312ad2dbeb23c75aa16fcea80a6b21f
Keefe Laporte <keefelaporte2019@gmail.com.com>
2019-09-30 21:44:18 -0400

    Objects Printing

---
+++ objects-in-array.js
@@ -0,0 +1,15 @@
+//var cars = ['BMW ', 'Benz ', 'Caddy '];
+let cars = [{
+    make: 'Beamer',
+    model: 'X5',
+    year: 2019
+},  {
+    make: 'Benz',
+    model: 'S63',
+    year: 2016
+},  {
+    make: 'Caddy',
+    model: 'Escalade',
+    year: 2016
+}]
+console.log(cars)
